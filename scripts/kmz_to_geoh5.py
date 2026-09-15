1a82a6d5bf36b59780761764b1657fbe49816ead
Derek Kinakin <89031238+derek-kinakin@users.noreply.github.com>
2026-09-15 10:56:57 -0700

    Minor edits to example script.

--- scripts/kmz_to_geoh5.py
+++ scripts/kmz_to_geoh5.py
@@ -6,7 +6,7 @@ Example script using the 'kmz2geoh5' library.
 from pathlib import Path
 
 # Import dev version of library
-import kmz2geoh5 as ck2g
+import kmz2geoh5 as k2g
 
 # KMZ file path
 KMZ_PATH = r"C:\Users\dkinakin\OneDrive - BGC Engineering Inc\DK SFdS_2026-09-08.kmz"
@@ -15,4 +15,4 @@ EPSG = 26712
 # Script
 kmz = Path(KMZ_PATH)
 geoh5_path = kmz.with_suffix(".geoh5")
-geoh5 = ck2g.convert(kmz, epsg=EPSG, geoh5_path=geoh5_path)
+geoh5 = k2g.convert(kmz, geoh5_path=geoh5_path, epsg=EPSG)
